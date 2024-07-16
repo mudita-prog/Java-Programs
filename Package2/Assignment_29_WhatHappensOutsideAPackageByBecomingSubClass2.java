e5bb45cf8bc50cdb42697dbc710c74bc2775b091
mudita-prog <muditagupta1994@gmail.com>
2024-07-16 15:19:51 +0530

    Assignment 29

---
+++ Package2/Assignment_29_WhatHappensOutsideAPackageByBecomingSubClass2.java
@@ -0,0 +1,16 @@
+package Package2;
+
+import Package1.Assignment_29_WhatHappensOutsideAPackageByBecomingSubClass1;
+
+public class Assignment_29_WhatHappensOutsideAPackageByBecomingSubClass2 extends Assignment_29_WhatHappensOutsideAPackageByBecomingSubClass1{
+
+	public static void main(String[] args) 
+	{
+		Assignment_29_WhatHappensOutsideAPackageByBecomingSubClass2 c1=new Assignment_29_WhatHappensOutsideAPackageByBecomingSubClass2();
+		c1.multiplication();
+		c1.sub();
+		
+
+	}
+
+}
---
+++ Package2/Assignment_30_WhatHappensoutsideAPackageWithoutBecomingSubClass2.java
@@ -0,0 +1,12 @@
+package Package2;
+
+import Package1.Assignment_30_WhatHappensoutsideAPackageWithoutBecomingSubClass1;
+
+public class Assignment_30_WhatHappensoutsideAPackageWithoutBecomingSubClass2 {
+
+	public static void main(String[] args) {
+		Assignment_30_WhatHappensoutsideAPackageWithoutBecomingSubClass1 c1 = new Assignment_30_WhatHappensoutsideAPackageWithoutBecomingSubClass1();
+		c1.sub();
+	}
+
+}
