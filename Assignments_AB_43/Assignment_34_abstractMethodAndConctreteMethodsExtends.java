ce6c477ddae648e5afbeb4c66451b52c7396e2ae
mudita-prog <muditagupta1994@gmail.com>
2024-07-19 19:55:04 +0530

    assignment

--- Assignments_AB_43/Assignment_34_abstractMethodAndConctreteMethodsExtends.java
+++ Assignments_AB_43/Assignment_34_abstractMethodAndConctreteMethodsExtends.java
@@ -1,47 +1,35 @@
 package Assignments_AB_43;
-abstract class Method3
+interface Grand_Parent
 {
-	abstract void add();
-	abstract void subtract();
-}	
-abstract class Method2 extends Method3
-{
-	void multiply()
-	{
-		System.out.println("3");
-	}
-	static void divide()
-	{
-		System.out.println("4");
-	}
+	void abstract_method1();
+	void abstract_method2();
 }
-public class Assignment_34_abstractMethodAndConctreteMethodsExtends extends Method2 
+interface Parent extends Grand_Parent 
 {
-	void percentage()
-	{
-		System.out.println("5");
-	}
-	static void root()
-	{
-		System.out.println("6");
-	}
-	void add()
-	{
-	System.out.println("1");
-	}
-	void subtract() 
-	{
-	System.out.println("2");
-	}
-public static void main(String[] args)
-{
-	Assignment_34_abstractMethodAndConctreteMethodsExtends a1=new Assignment_34_abstractMethodAndConctreteMethodsExtends();
-	a1.add();
-	a1.multiply();
-	a1.subtract();
-	a1.percentage();
-	divide();
-	root();
+	void abstract_method3();
+	void abstract_method4();
 }
+public class Assignment_34_abstractMethodAndConctreteMethodsExtends implements Parent 
+{	
+public static void main(String[] args) {
+	Assignment_34_abstractMethodAndConctreteMethodsExtends s1 = new Assignment_34_abstractMethodAndConctreteMethodsExtends();
+	s1.abstract_method1();
+	s1.abstract_method2();
+	s1.abstract_method3();
+	s1.abstract_method4();
 
 }
+public void abstract_method1() 
+{
+	System.out.println("1");
+}
+public void abstract_method2() {	
+	
+System.out.println("1");
+}
+public void abstract_method3() {
+	System.out.println("1");
+}
+public void abstract_method4() {
+	System.out.println("1");
+}}
